f844c014d4c8ca109c17ecf8d7714c98dc57a29d
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-29 12:27:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,25 +9,3 @@ navList.classList.toggle('v-class');
 navbar.classList.toggle('h-nav');
 })
 
-
-
-
-
-
-const about = document.getElementById("about");
-const whyJapan = document.getElementById("why-japan");
-const japanWork = about.querySelector("japanW-work");
-
-// about.addEventListener("click", (e) => {
-//   whyJapan.classList.toggle("open");
-
-//   const isOpen = whyJapan.classList.contains("open");
-//   japanWork.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
-// });
-
-// whyJapan.addEventListener("click", (e) => {
-//   whyJapan.classList.remove("open");
-//   japanWork.setAttribute("class", "ri-menu-line");
-// });
-
-
